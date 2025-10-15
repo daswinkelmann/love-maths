640714e8a458acef2dfa38bd480548cfbbb0b7e0
daswinkelmann <daswinkelmann@gmail.com>
2025-10-15 13:56:58 +0100

    Calculating The Correct Answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
 // Wait for the DOM to finish loading before running the game
 // Get the button elements and add event listeners to them
 
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
 
     for (let button of buttons) {
-        button.addEventListener("click", function () {
+        button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "submit") {
                 alert("You clicked Submit!");
             } else {
@@ -14,8 +14,10 @@ document.addEventListener("DOMContentLoaded", function () {
             }
         });
     }
+
   runGame("addition");  
-})
+
+});
 
 /**
  * The main game "loop", called when the script is first loaded
@@ -30,21 +32,49 @@ function runGame(gameType) {
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
     } else {
-        alert(`Unkown game type: ${gameType}`);
-        throw `Unkown game type: ${gametype}. Aborting!`;
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
     }
 
-    //Check these two curly brackets
+}
+
+function checkAnswer() {
+
+}
+
+/**
+ * Gets the operands (the numbers) and the operator (plus, minus etc)
+ * directly from the dom, and returns the correct answer.
+ */
+
+function calculateCorrectAnswer() {
+
+    let operand1 = parseInt(document.getElementById('operand1').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
+    let operator = document.getElementById("operator").innerText;
+
+    if (operator === "+") {
+        return [operand1 + operand2, "addition"];
+    } else {
+        alert(`Unimplemented operator ${operator}`);
+        throw `Unimplemented operator ${operator}. Aborting!`;
+
     }
-// }
+    }
 
-function checkAnswer() {}
+// check for random curly
+    // }
 
-function calculateCorrectAnswer() {}
 
-function incrementScore() {}
 
-function incrementWrongAnswer() {}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
 
 function displayAdditionQuestion(operand1, operand2) {
 
@@ -53,9 +83,13 @@ function displayAdditionQuestion(operand1, operand2) {
     document.getElementById('operator').textContent = "+";
 }
 
-function displaySubtractQuestion() {}
+function displaySubtractQuestion() {
+    
+}
 
-function displayMultiplyQuestion() {}
+function displayMultiplyQuestion() {
+
+}
 
 // function displayDivideQuestion() {
 
