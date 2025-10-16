c90739694786279a4b539f5f4ec7b78e181a809c
daswinkelmann <daswinkelmann@gmail.com>
2025-10-16 13:01:58 +0100

    add favicon and final tidy ups

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,6 +15,14 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 
+    document
+        .getElementById("answer-box")
+        .addEventListener("keydown", function (event) {
+            if (event.key === "Enter") {
+                checkAnswer();
+            }
+        });
+
     runGame("addition");
 });
 
@@ -23,6 +31,9 @@ document.addEventListener("DOMContentLoaded", function () {
  * and after the user's answer has been processed
  */
 function runGame(gameType) {
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
+
     // Creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
@@ -75,7 +86,7 @@ function calculateCorrectAnswer() {
 
     if (operator === "+") {
         return [operand1 + operand2, "addition"];
-    } else if (operator === "X") {
+    } else if (operator === "x") {
         return [operand1 * operand2, "multiply"];
     } else if (operator === "-") {
         return [operand1 - operand2, "subtract"];
@@ -114,14 +125,14 @@ function displaySubtractQuestion(operand1, operand2) {
     document.getElementById("operand1").textContent =
         operand1 > operand2 ? operand1 : operand2;
     document.getElementById("operand2").textContent =
-        operand1 > operand2 ? operand1 : operand2;
+        operand1 > operand2 ? operand2 : operand1;
     document.getElementById("operator").textContent = "-";
 }
 
 function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById("operand1").textContent = operand1;
     document.getElementById("operand2").textContent = operand2;
-    document.getElementById("operator").textContent = "X";
+    document.getElementById("operator").textContent = "x";
 }
 
 // function displayDivideQuestion() {
